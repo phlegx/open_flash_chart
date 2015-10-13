ea03de60ba09e5217180c9f1777f95006b7d521f
Felix Schäfer <felix@plan.io>
2015-10-13 09:57:47 +0200

    Make this a Redmine plugin to automatically copy assets #232252

--- init.rb
+++ init.rb
@@ -7,3 +7,7 @@ ActionController::Base.send :include, OpenFlashChart
 ActiveRecord::Base.send :include, OpenFlashChart::View
 ActiveRecord::Base.send :include, OpenFlashChart::Controller
 ActiveRecord::Base.send :include, OpenFlashChart
+
+Redmine::Plugin.register :open_flash_chart do
+  hidden true
+end
